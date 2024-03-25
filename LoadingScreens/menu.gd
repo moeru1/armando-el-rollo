bdb915a0c72aecc3d7e20a4f5fa191ec8fe58ca2
Riczap <riczap06@gmail.com>
2024-03-25 17:56:48 -0600

    Menu update

--- LoadingScreens/menu.gd
+++ LoadingScreens/menu.gd
@@ -1,6 +1,28 @@
 extends CanvasLayer
-@onready var button_sound = $Panel/Button/ButtonSound
-func _on_button_pressed():
+@onready var button_sound = $ButtonSound
+
+func _on_play_button_pressed():
 	button_sound.play()
 	LoadManager.load_scene("res://Scenes/World/level.tscn")
 	MusicPlayer.play_song(GlobalValues.gameplay_music_path)
+
+
+func _on_quests_button_pressed():
+	button_sound.play()
+
+
+func _on_store_button_pressed():
+	button_sound.play()
+
+
+func _on_profile_button_pressed():
+	button_sound.play()
+
+
+func _on_settings_button_pressed():
+	button_sound.play()
+
+
+func _on_exit_button_pressed():
+	button_sound.play()
+	get_tree().quit()
